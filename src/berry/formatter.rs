e76fb5306b385dc46dc799e0c678d8a75da71013
Amalie <amalie.bjorgen@bkk.no>
2024-04-08 21:42:36 +0200

    english berry effect

--- src/berry/formatter.rs
+++ src/berry/formatter.rs
@@ -3,9 +3,14 @@ use crate::item::structs::Item;
 
 pub fn beautify_berry_output(berry_item: (Berry, Item)) {
     let (berry, item) = berry_item;
+    let english_effect = item
+        .effect_entries
+        .iter()
+        .find(|effect| effect.language.name == "en")
+        .unwrap();
     println!("---------------------------------------");
     println!("Name: {}", item.name);
     println!("Natural Gift Type: {}", berry.natural_gift_type.name);
-    println!("Effect: {}", item.effect_entries[0].short_effect);
+    println!("Effect: {}", english_effect.short_effect);
     println!("---------------------------------------");
 }
